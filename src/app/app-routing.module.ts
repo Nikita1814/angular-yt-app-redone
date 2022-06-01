532433a7ba49f7ab6161d487914f3cd8289d35c3
Nikita1814 <kravchenko1814@gmail.com>
2022-06-01 21:03:02 +0300

    feat: set up auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./youtube/youtube.module').then((m) => m.YoutubeModule),
+  },
+  { path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/guards/auth-req.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { User } from '../auth/models/auth-models';
+import { getUser } from '../redux/auth-reducer/auth.selector';
+import { PageState } from '../redux/state-related-models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthReqGuard implements CanActivate {
+  constructor ( private store: Store<PageState>, public router: Router) {
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const userCards$ = this.store.select(getUser);
+      if (!userCards$.subscribe((u: User) => u.token)) {
+        this.router.navigate(['auth/']);
+        return false;
+      } else {
+        return true;
+      }
+  }
+
+}
