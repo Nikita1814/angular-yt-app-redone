f5bcc8c882dd3e038ad07661a0db8b24b8c4d1f1
Nikita1814 <kravchenko1814@gmail.com>
2022-06-15 14:50:39 +0300

    move fitlering and sorting from the reducer actions into the selector

--- src/app/redux/search-item-reducer/search-item.reducer.ts
+++ src/app/redux/search-item-reducer/search-item.reducer.ts
@@ -2,12 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { createReducer, on } from '@ngrx/store';
 import { ResponseVideo } from 'src/app/youtube/models/yt-models';
 import sortFuncs from 'src/app/youtube/utils/filter-functions';
-import {
-  clearSearchItemsError,
-  searchItemsError,
-  updateFilters,
-  updateSearchItems,
-} from './search-item.actions';
+import { clearSearchItemsError, searchItemsError, updateFilters, updateSearchItems } from './search-item.actions';
 
 export type SortType = 'date' | 'views' | 'likes' | 'none';
 export interface Filters {
@@ -16,57 +11,40 @@ export interface Filters {
 }
 export interface SearchItemState extends Filters {
   searchItems: ResponseVideo[];
-  filteredSearchItems: ResponseVideo[];
-  searchItemsError: HttpErrorResponse | null;
+  searchItemsError: HttpErrorResponse | null
 }
 //export const initialState: ResponseVidInt[] | [] = [];
 export const initialState: SearchItemState = {
   searchItems: [],
-  filteredSearchItems: [],
   sortType: 'none',
   filterBy: '',
-  searchItemsError: null,
+  searchItemsError: null
 };
 
 export const searchItemsReducer = createReducer(
   initialState,
-
   on(
     updateSearchItems,
     (state, { searchItems }): SearchItemState => ({
       ...state,
       searchItems: searchItems,
-      filteredSearchItems: searchItems
-        .filter((el) => el.snippet.title.toLowerCase().includes(state.filterBy))
-        .sort(sortFuncs[state.sortType]),
     })
   ),
-
   on(
     updateFilters,
     (state, props): SearchItemState => ({
       ...state,
-      filteredSearchItems: state.searchItems
-        .filter((el) => el.snippet.title.toLowerCase().includes(props.filterBy))
-        .sort(sortFuncs[props.sortType]),
       sortType: props.sortType,
       filterBy: props.filterBy,
     })
   ),
+  on(searchItemsError, (state, props): SearchItemState => ({
+  ...state,
+  searchItemsError: props.error
+  })),
 
-  on(
-    searchItemsError,
-    (state, props): SearchItemState => ({
-      ...state,
-      searchItemsError: props.error,
-    })
-  ),
-
-  on(
-    clearSearchItemsError,
-    (state): SearchItemState => ({
-      ...state,
-      searchItemsError: null,
-    })
-  )
+  on(clearSearchItemsError, (state) : SearchItemState => ({
+    ...state,
+    searchItemsError:null
+  }))
 );
